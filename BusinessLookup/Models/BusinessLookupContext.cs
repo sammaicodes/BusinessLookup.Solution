00906d5172f0ec44bb2c327a974daa010f518480
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-06-11 09:54:37 -0700

    Add more files and content to program, startup, & Context.

---
+++ BusinessLookup/Models/BusinessLookupContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLookup.Models
+{
+  public class BusinessLookupContext : DbContext
+  {
+    public BusinessLookupContext(DbContextOptions<BusinessLookupContext> options)
+        : base(options)
+        {
+        }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<LocalBusiness>()
+        .HasData(
+          new LocalBusiness { LocalBusinessId = 1, BusinessType = "Restaurant", BusinessName = "Pok Pok", Review = "I love this restautant. It is SE Asian inspired and the food is sooo delicious. The only downside is you do have to wait a long time", RateReview = 4 },
+          new LocalBusiness { LocalBusinessId = 2, BusinessType = "Restaurant", BusinessName = "Loving Hut", Review = "Even though i'm not vegan, the food here is absolutely delicious. I've eaten here numerous times and everything I've tryied is mouthwatering", RateReview = 5 },
+          new LocalBusiness { LocalBusinessId = 2, BusinessType = "Bar", BusinessName = "Chowder Tavern", Review = "Its a must. All their drinks are super different. I had the clammy blody mary and it was one of the best I've had", RateReview = 4 },
+          new LocalBusiness { LocalBusinessId = 3, BusinessType = "Flowe Shop", BusinessName = "Happy Orchid", Review = "They have a wide veriety of flowers and they also offer classes with a pretty awesome instructor", RateReview = 5 },
+          new LocalBusiness { LocalBusinessId = 4, BusinessType = "Hardware Shop", BusinessName = "Crazy Hammer", Review = "Its your regular well-stock hardware store. But their staff is limited and hard to get help.", RateReview = 3 },
+          new LocalBusiness { LocalBusinessId = 5, BusinessType = "Grocery Shop", BusinessName = "Sour Piv   ", Review = "You have to visit for yourself. The food was really good in general. The valleys were breathtaking...", RateReview = 5 }
+        );
+    }
+    public DbSet<LocalBusiness> LocalBusinesses { get; set; }
+  }
+}

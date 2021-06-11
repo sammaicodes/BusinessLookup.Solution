5c297760c6b57a07f74c01156438c87f1c0d25d5
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-06-11 11:43:16 -0700

    Fix the issue.

--- BusinessLookup/Migrations/20210611183942_Initial.cs
+++ BusinessLookup/Migrations/20210611183942_Initial.cs
@@ -22,6 +22,19 @@ namespace BusinessLookup.Migrations
                 {
                     table.PrimaryKey("PK_LocalBusinesses", x => x.LocalBusinessId);
                 });
+
+            migrationBuilder.InsertData(
+                table: "LocalBusinesses",
+                columns: new[] { "LocalBusinessId", "BusinessName", "BusinessType", "RateReview", "Review" },
+                values: new object[,]
+                {
+                    { 1, "Pok Pok", "Restaurant", 4, "I love this restautant. It is SE Asian inspired and the food is sooo delicious. The only downside is you do have to wait a long time" },
+                    { 2, "Loving Hut", "Restaurant", 5, "Even though i'm not vegan, the food here is absolutely delicious. I've eaten here numerous times and everything I've tryied is mouthwatering" },
+                    { 3, "Chowder Tavern", "Bar", 4, "Its a must. All their drinks are super different. I had the clammy blody mary and it was one of the best I've had" },
+                    { 4, "Happy Orchid", "Flowe Shop", 5, "They have a wide veriety of flowers and they also offer classes with a pretty awesome instructor" },
+                    { 5, "Crazy Hammer", "Hardware Shop", 3, "Its your regular well-stock hardware store. But their staff is limited and hard to get help." },
+                    { 6, "Freemont", "Grocery Shop", 5, "Even though its a small neighborhood store, it is very well stocked, you can find just about anything" }
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
--- BusinessLookup/Models/BusinessLookupContext.cs
+++ BusinessLookup/Models/BusinessLookupContext.cs
@@ -15,10 +15,10 @@ namespace BusinessLookup.Models
         .HasData(
           new LocalBusiness { LocalBusinessId = 1, BusinessType = "Restaurant", BusinessName = "Pok Pok", Review = "I love this restautant. It is SE Asian inspired and the food is sooo delicious. The only downside is you do have to wait a long time", RateReview = 4 },
           new LocalBusiness { LocalBusinessId = 2, BusinessType = "Restaurant", BusinessName = "Loving Hut", Review = "Even though i'm not vegan, the food here is absolutely delicious. I've eaten here numerous times and everything I've tryied is mouthwatering", RateReview = 5 },
-          new LocalBusiness { LocalBusinessId = 2, BusinessType = "Bar", BusinessName = "Chowder Tavern", Review = "Its a must. All their drinks are super different. I had the clammy blody mary and it was one of the best I've had", RateReview = 4 },
-          new LocalBusiness { LocalBusinessId = 3, BusinessType = "Flowe Shop", BusinessName = "Happy Orchid", Review = "They have a wide veriety of flowers and they also offer classes with a pretty awesome instructor", RateReview = 5 },
-          new LocalBusiness { LocalBusinessId = 4, BusinessType = "Hardware Shop", BusinessName = "Crazy Hammer", Review = "Its your regular well-stock hardware store. But their staff is limited and hard to get help.", RateReview = 3 },
-          new LocalBusiness { LocalBusinessId = 5, BusinessType = "Grocery Shop", BusinessName = "Freemont", Review = "Even though its a small neighborhood store, it is very well stocked, you can find just about anything", RateReview = 5 }
+          new LocalBusiness { LocalBusinessId = 3, BusinessType = "Bar", BusinessName = "Chowder Tavern", Review = "Its a must. All their drinks are super different. I had the clammy blody mary and it was one of the best I've had", RateReview = 4 },
+          new LocalBusiness { LocalBusinessId = 4, BusinessType = "Flowe Shop", BusinessName = "Happy Orchid", Review = "They have a wide veriety of flowers and they also offer classes with a pretty awesome instructor", RateReview = 5 },
+          new LocalBusiness { LocalBusinessId = 5, BusinessType = "Hardware Shop", BusinessName = "Crazy Hammer", Review = "Its your regular well-stock hardware store. But their staff is limited and hard to get help.", RateReview = 3 },
+          new LocalBusiness { LocalBusinessId = 6, BusinessType = "Grocery Shop", BusinessName = "Freemont", Review = "Even though its a small neighborhood store, it is very well stocked, you can find just about anything", RateReview = 5 }
         );
     }
     public DbSet<LocalBusiness> LocalBusinesses { get; set; }
